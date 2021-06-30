83db1bc22fe23a042d11679b38fdcbf5145a6751
maldrek3103 <cpisanu96@gmail.com>
2021-06-30 15:42:26 +0200

    created variables, added prompt

--- js/script.js
+++ js/script.js
@@ -1,1 +1,21 @@
-console.log('hello world')
+var firstName;
+var lastName;
+var favColour;
+var pswEnding = 21;
+var generatedPsw;
+
+
+
+firstName = prompt('What is your first name?', 'Thomas');
+lastName = prompt('What is your last name?', 'Edison');
+favColour = prompt('What is your favourite colour?', 'Red');
+generatedPsw = firstName + lastName + favColour + pswEnding;
+
+
+
+
+
+console.log(firstName)
+console.log(lastName)
+console.log(favColour)
+console.log(generatedPsw)
