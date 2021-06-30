05570f53979e8fd7b8d00e0305307cd25d751c8b
maldrek3103 <cpisanu96@gmail.com>
2021-06-30 16:09:51 +0200

    password generator finished

--- js/script.js
+++ js/script.js
@@ -1,21 +1,21 @@
-var firstName;
-var lastName;
-var favColour;
+// Variabile fissa
 var pswEnding = 21;
-var generatedPsw;
 
+// Recuperiamo i dati dell'utente
+var firstName = prompt('What is your first name?', 'Thomas');
+var lastName = prompt('What is your last name?', 'Edison');
+var favColour = prompt('What is your favourite colour?', 'Red');
 
+// Password generata dagli input dell'utente
+var generatedPsw = firstName + lastName + favColour + pswEnding;
 
-firstName = prompt('What is your first name?', 'Thomas');
-lastName = prompt('What is your last name?', 'Edison');
-favColour = prompt('What is your favourite colour?', 'Red');
-generatedPsw = firstName + lastName + favColour + pswEnding;
+// Creiamo una variabile per recuperare l'elemento HTML
+var passwordPlacheholder = document.getElementById('password');
 
+// Inseriamo la password nell'elemento HTML selezionato
+passwordPlacheholder.innerHTML = generatedPsw;
 
-
-
-
-console.log(firstName)
-console.log(lastName)
-console.log(favColour)
-console.log(generatedPsw)
+// console.log(firstName);
+// console.log(lastName);
+// console.log(favColour);
+// console.log(generatedPsw);
